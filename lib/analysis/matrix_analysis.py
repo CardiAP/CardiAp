ef4ecc03bfb6b628d503475760b5ad1ed262de12
leandrosommese <sommeseleandro@gmail.com>
2022-11-08 09:50:14 -0300

    file ignored

--- lib/analysis/matrix_analysis.py
+++ lib/analysis/matrix_analysis.py
@@ -9,7 +9,7 @@ from lib.image.image_data import split_vertically_by
 
 def analyze_image(image, min_dist_between_max_peaks, calibration, slice_width=0):
     image_analysis = _analyze_matrix(image, min_dist_between_max_peaks, calibration)
-    slices_analysis = [_analyze_matrix_slice(matrix, min_dist_between_max_peaks, image_analysis, calibration) for matrix in
+    slices_analysis = [_analyze_matrix(matrix, min_dist_between_max_peaks, calibration) for matrix in
                        split_vertically_by(image, slice_width)] if slice_width > 0 else []
     
     return ({
